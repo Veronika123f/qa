589a319c9ae1519888602e85485067c63bf178d8
Veronika123f <132203483+Veronika123f@users.noreply.github.com>
2023-04-30 14:55:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string octal1 = "56";
+        string octal2 = "2";
+
+        int decimal1 = Convert.ToInt32(octal1, 8);
+        int decimal2 = Convert.ToInt32(octal2, 8);
+        int resultDecimal = decimal1 / decimal2;
+
+        string resultOctal = Convert.ToString(resultDecimal, 8);
+
+        Console.WriteLine("Result: " + resultOctal);
+        Console.ReadLine();
+    }
+}
+    
+
+
